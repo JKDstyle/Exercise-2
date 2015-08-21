aa88f23a5c6334f97512ed2a6a57ce29946847f1
Alex Perales <alexperalesroca@gmail.com>
2015-08-21 14:36:05 +0200

    ejercicio chees

--- MovieRating.rb
+++ MovieRating.rb
@@ -1,25 +1,47 @@
 require 'imdb'
 
+
 class GetFilms
 # 	def intialize(array_of_films)
 # 		@array_of_films = []
 		
 # 	end
 
-	def fetch_film(array_of_film)
-		@title_array = []
-		array_of_film.each do|x|
-			newmovie = Imdb::Search.new(x).movies[0].rating
-			@title_array.push(newmovie)
+	def show_films(array_of_films)
+		array_of_films.each do |movie|
+			print " El nombre de mi pelicula es "
+			print movie
+			print " El rating es de "
+			newmovierating = Imdb::Search.new(movie).movies[0].rating
+			print newmovierating
+			puts "\n"
+			show_director = Imdb::Search.new(movie).movies[0].director
+			print  
+			puts " #{show_director[0]} is the director"
+			puts "\n"
+			
+			
 		end
-		puts @title_array
 	end 
 
-	def search_movie_id
-			
+	def make_a_rating_board
+			10.times do
+				print "| " * 7
+				puts "|"
+			end
+	end
+
+	def make_a_rating_board_of_numbers
+			7.times do |i|
+				print "|#{i+1}" 
+
+			end
+			puts "|"
 	end
 
 end
  
 	my_fetcher = GetFilms.new
-	puts my_fetcher.fetch_film(["Ghostbuster", "X-men", "Titanic"])
+	my_fetcher.show_films(["X-men", "Titanic"])
+	my_fetcher.make_a_rating_board
+	my_fetcher.make_a_rating_board_of_numbers
